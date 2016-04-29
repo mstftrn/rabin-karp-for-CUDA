1cfcc83cd038f301487d1cdaae75f1c045a1428f
mstftrn <mustafaturan@Mustafas-MacBook-Pro.local>
2016-04-29 22:13:19 +0300

    Working matrix

--- rabinomp.c
+++ rabinomp.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <omp.h>
+//#include <omp.h>
 #include <string.h>
 #include <math.h>
 /*
@@ -20,18 +20,56 @@ int compute_value(char *str, int length, int d, int q)
 
 int rk_matcher(char *str, char *pattern, int d, int q)
 {
-    int i = 0;
+    int i = 0,j=0;
     int str_length = strlen(str);
     int pattern_length = strlen(pattern);
     int p0 = 0;
     int ts[str_length];
+
     int num_cores=4;
     int chunk_len=(int)ceil((float)str_length/num_cores);
     int padding_len=chunk_len*num_cores-str_length;
     int el_chunk_len=chunk_len+pattern_length-1;
+
+    //matrix which holds the characters, each row will go to a core
     int tss[num_cores][el_chunk_len];
-    for (int i=0; i<pattern_length-1; i++)
+    
+    //initial zeroes
+    for (i=0; i<pattern_length-1; i++)
     	tss[0][i]=0;
+
+    //first n-1 cores' characters
+    for (i=0; i<num_cores-1; i++)
+        for (j=0;j<chunk_len;j++)
+            tss[i][j+pattern_length-1]=str[i*chunk_len+j];
+    
+    //last core's characters
+    for (i=(num_cores-1)*chunk_len, j=0; i<str_length;i++,j++)
+        tss[num_cores-1][j+pattern_length-1]=str[i];
+    
+    //last n-1 cores' padding characters
+    for (i=1;i<num_cores;i++)
+        for (j=0;j<pattern_length-1;j++)
+            tss[i][j]=tss[i-1][j+chunk_len];
+    
+    //last core's last paddings
+    for (i=0; i<padding_len;i++)
+        tss[num_cores-1][el_chunk_len-i-1]=0;
+
+    /* This code block prints what is inside the matrix
+    for (i=0;i<num_cores;i++)
+    {
+        for (j=0;j<el_chunk_len;j++)
+            if (tss[i][j]==0)
+                printf("%c", '0');
+            else
+                printf("%c", tss[i][j]);
+        printf("\n");
+    }
+    */
+
+
+
 
     p0 = compute_value(pattern, pattern_length, d, q);
     
@@ -50,7 +88,6 @@ int rk_matcher(char *str, char *pattern, int d, int q)
     	printf("%d ", ts[i]);
     }
 
-    int j = 0;
     for (i = 0; i <= str_length - pattern_length+1; ++i) {
 	if (ts[i] == p0) {
 	    for (j = 0; j < pattern_length; ++j) {
@@ -69,7 +106,7 @@ int rk_matcher(char *str, char *pattern, int d, int q)
 
 int main(int argc, char *argv[])
 {
-    int pos = rk_matcher("1234567", "78", 10, 500000);
+    int pos = rk_matcher("bababanaparaver", "aba", 10, 500000);
     //printf("%d", pos);
     return 0;
 }
